ebdbcc879d8e70489523aa36ef9b3cafcec132ca
chenzhijun <522858454@qq.com>
2015-09-18 21:36:32 +0800

    增加了删除servlet

---
+++ src/com/inews/servlet/DeleteMenuById.java
@@ -0,0 +1,79 @@
+package com.inews.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.inews.utils.DbCRUD;
+
+public class DeleteMenuById extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public DeleteMenuById() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+			this.doPost(request, response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+			String menuId=request.getParameter("menu_id");
+			System.out.println(menuId);
+			int id=Integer.parseInt(menuId);
+			DbCRUD db=new DbCRUD();
+			String sql="DELETE FROM menu WHERE menuid=?;";
+			int result=(Integer)db.delete(sql, id);
+			db.releaseConn();
+			if(result>0){
+				response.sendRedirect("admin/menu_query_body.jsp");
+			}else{
+				request.getRequestDispatcher("error.jsp").forward(request, response);
+			}
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
