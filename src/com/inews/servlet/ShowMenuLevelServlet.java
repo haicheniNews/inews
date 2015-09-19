7b64ba06052e66295af1309902eb3491b851c9a9
chenzhijun <522858454@qq.com>
2015-09-19 10:32:11 +0800

    增加菜单时,显示父菜单等级

---
+++ src/com/inews/servlet/ShowMenuLevelServlet.java
@@ -0,0 +1,87 @@
+package com.inews.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.inews.utils.DbCRUD;
+
+public class ShowMenuLevelServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public ShowMenuLevelServlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		this.doPost(request, response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+
+		String fatherLevel=request.getParameter("father_level");
+		DbCRUD db=new DbCRUD();
+		String sql="SELECT * FROM menu WHERE menulevel=?;";
+		List<Map<String,Object>> list=(ArrayList<Map<String,Object>>)db.query(sql, Integer.parseInt(fatherLevel));
+		JSONObject json=new JSONObject();
+		JSONArray arr = new JSONArray(list);
+		
+		System.out.println("--------jsonArray------" + arr.toString());
+		response.getWriter().write(arr.toString());
+		
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
