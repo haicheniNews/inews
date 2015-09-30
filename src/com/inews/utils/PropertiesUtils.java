b0e8b53bf2bfa19b08b52837c42b7d1d48ea1a70
chenzhijun <522858454@qq.com>
2015-09-30 12:46:28 +0800

    测试通过了.

--- src/com/inews/utils/PropertiesUtils.java
+++ src/com/inews/utils/PropertiesUtils.java
@@ -1,5 +1,6 @@
 package com.inews.utils;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
@@ -7,6 +8,12 @@ public class PropertiesUtils {
 	public static String getFilePath(){
 		InputStream in = DbUtils.class.getResourceAsStream("database.properties");
 		Properties p = new Properties();
+		try {
+			p.load(in);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		String value=p.getProperty("file_path");
 		return value;
 		
