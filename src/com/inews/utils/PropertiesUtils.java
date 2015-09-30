edeba43afa84f111c43852bfba35342fe86eb7ab
chenzhijun <522858454@qq.com>
2015-09-30 12:43:28 +0800

    增加配置文件解析类

---
+++ src/com/inews/utils/PropertiesUtils.java
@@ -0,0 +1,15 @@
+package com.inews.utils;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesUtils {
+	public String getFilePath(){
+		InputStream in = DbUtils.class.getResourceAsStream("database.properties");
+		Properties p = new Properties();
+		String value=p.getProperty("file_path");
+		return value;
+		
+	}
+
+}
